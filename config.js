f82b6e531481d6b70ca41057448ae50612c64c33
kutyel <flaviocorpa@gmail.com>
2016-05-18 23:09:26 +0200

    Restablish moment.js

--- config.js
+++
@@ -1,8 +0,0 @@
-System.config({
-  packages: {
-    src: {
-      format: 'register',
-      defaultExtension: 'js'
-    }
-  }
-});
--- src/ng2-timer.ts
+++ src/ng2-timer.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit, Input } from "angular2/core";
-import { DateFormatPipe } from "angular2-moment";
 import { Observable } from "rxjs/Rx";
+import moment from "moment";
 
 @Component({
     selector: "ng-timer",
-    pipes: [DateFormatPipe],
-    template: `{{ timer | async | amDateFormat: format }}`,
+    template: `{{ time }}`,
     inputs: ["startTime", "format"]
 })
 export class TimerComponent implements OnInit {
 
-    private timer: Observable<number>;
+    public time: string;
 
     @Input()
     format: string;
@@ -19,6 +18,8 @@ export class TimerComponent implements OnInit {
     startTime: number = 1000;
 
     ngOnInit() {
-        this.timer = Observable.timer(this.startTime, 1);
+        Observable
+            .timer(this.startTime, 10)
+            .subscribe(t => this.time = moment(t).format(this.format));
     }
 }
